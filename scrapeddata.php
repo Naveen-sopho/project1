ca191d8ca642136ea166badcc79cebbc396eddad
Naveen Nani <palnatynaveen1998@gmail.com>
2017-03-14 17:06:30 +0000

    facilities and looping

--- scrapeddata.php
+++ scrapeddata.php
@@ -1,56 +1,57 @@
 <?php
-require("mysqlconnect.php");
-//$dbconnect = mysqli_connect('localhost', 'naveen_palnaty', 'v1kCjsvLYytrBTGV','project1');
-////if (!$dbconnect) {
-    //die('Could not connect: ' . mysql_error());
+
 //}
+
 if (empty($_GET['q']))
 {
-    echo "Please Enter URL.";
-    exit;
-    
+  echo "Please Enter URL.";
+  exit;
+
 }
 $query = $_GET['q'];
 
 $wet=file_get_contents($_GET['q']);
-//echo $wet;
+$wet = preg_replace("/\n/i", "", $wet);
+print_r("<pre>");
+preg_match_all('/<div class="clg-tpl-parent">(.*?)<input type="hidden"/i', $wet, $matches);
+//array declaration
+$c = [];
+$college_name = [];
+$ca = [];
+$college_address = [];
+$f = [];
+$facilities = [];
+//storing scraped data
+for($j=0;$j<30;$j++)
+{
+ 
+  preg_match_all('@<h2 class="tuple-clg-heading"><a href="[^"]+" target="[^"]+">\s*([^<]+)<\/a>@', $matches[0][$j], $c); 
+  preg_match_all('@<p>\|\s*([^<]+)<\/p><\/h2>@' , $matches[0][$j], $ca);
+  preg_match_all('@<div class="srpHoverCntnt2"><h3>\s*([^<]+)<\/h3>@', $matches[0][$j], $f);
+  $college_name[$j] = $c[1][0];
+  $college_address[$j] = $ca[1][0];
+  $facilities[$j] = implode("|", $f[1]);
+}
+//checking arrays
+print_r($college_name);
+print_r($college_address);
+print_r($facilities);
 
-$regex='@<h2 class="tuple-clg-heading"><a href="[^"]+" target="[^"]+">\s*([^<]+)<\/a>\n<p>\|\s*([^<]+)<\/p><\/h2>@';
-$regex2='@<div class="srpHoverCntnt2">\n<h3>\s*([^<]+)<\/h3>@';
-$regex3='@<div class="tuple-revw-sec">\n<span><b>\s*([^<]+)<\/b><a target="[^"]+"\stype="reviews"@';
-if (!preg_match_all($regex, $wet, $matches)) 
-{
-  echo "No matches found";
-  exit;
-}
-if (!preg_match_all($regex2, $wet, $faci)) 
-{
-  echo "No matches found";
-  exit;
-}
-if (!preg_match_all($regex3, $wet, $review)) 
-{
-  echo "No matches found";
-  exit;
-}
 
-$titles = $matches[1];
-$college_address = $matches[2];
 
-$college_facilities = $faci[1];
-$college_reviews = $review[1];
-echo "<b>The following titles matched your query for '" . htmlspecialchars($query) . "':</b><br />";
-foreach($titles as $title) 
-{
-  mysqli_query($dbconnect,"SELECT * FROM colleges");
-  mysqli_query($dbconnect,"INSERT INTO colleges (college_name) VALUES(\"".$title."\")");
-
-}
-foreach($college_address as $add)
-{
-    mysqli_query($dbconnect,"SELECT * FROM college_address");
-    mysqli_query($dbconnect,"INSERT INTO college_address (college_add) VALUES(\"".$add."\")");
-}
-  mysqli_close($dbconnect);
+//echo "<b>The following titles matched your query for '" . htmlspecialchars($query) . "':</b><br />";
+//foreach($titles as $title) 
+// {
+//  mysqli_query($dbconnect,"SELECT * FROM colleges");
+//  mysqli_query($dbconnect,"INSERT INTO colleges (college_name) VALUES(\"".$title."\")");
+//
+//}
+//foreach($college_address as $add)
+//{
+//   mysqli_query($dbconnect,"SELECT * FROM college_address");
+//    mysqli_query($dbconnect,"INSERT INTO college_address (college_add) VALUES(\"".$add."\")");
+//}
+//  mysqli_close($dbconnect);
+//
 
 ?>
