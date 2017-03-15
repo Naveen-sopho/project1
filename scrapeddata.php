44e579195983a1c63fc3439626706a3c81159556
Naveen Nani <palnatynaveen1998@gmail.com>
2017-03-15 13:24:24 +0000

    fixing bugs

--- scrapeddata.php
+++ scrapeddata.php
@@ -8,8 +8,9 @@ if (empty($_GET['q']))
   exit;
 
 }
+mysqli_query($dbconnect, "TRUNCATE TABLE colleges");
 $query = $_GET['q'];
-
+echo $query;
 $wet=file_get_contents($_GET['q']);
 $wet = preg_replace("/\n/i", "", $wet);
 print_r("<pre>");
@@ -21,8 +22,9 @@ $ca = [];
 $college_address = [];
 $f = [];
 $facilities = [];
+$size = sizeof($matches[0],1);
 //storing scraped data
-for($j=0;$j<30;$j++)
+for($j=0;$j<$size;$j++)
 {
  
   preg_match_all('@<h2 class="tuple-clg-heading"><a href="[^"]+" target="[^"]+">\s*([^<]+)<\/a>@', $matches[0][$j], $c); 
@@ -33,9 +35,11 @@ for($j=0;$j<30;$j++)
   $facilities[$j] = implode("|", $f[1]);
 }
 //array declaration
+
+print_r($matches);
 $r = [];
 $reviews = [];
-for($k=0;$k<30;$k++)
+for($k=0;$k<$size;$k++)
 {
   preg_match_all('/<div class="tuple-revw-sec"><span><b>\s*([^<]+)<\/b><a target="[^"]+"\stype="reviews/i',$matches[0][$k],$r);
 
@@ -45,14 +49,18 @@ for($k=0;$k<30;$k++)
     $reviews[$k] = $r[1][0];
 }
 //checking arrays
-print_r($college_name);
-print_r($college_address);
-print_r($facilities);
-print_r($reviews);
-for($s=0;$s<30;$s++)
+//print_r($college_name);
+//print_r($college_address);
+//print_r($facilities);
+//print_r($reviews);
+
+for($s=0;$s<$size;$s++)
 {
     mysqli_query($dbconnect,"SELECT * FROM colleges");
     mysqli_query($dbconnect,"INSERT INTO colleges (college_name,college_address,facilities,reviews) VALUES(\"".$college_name[$s]."\",\"".$college_address[$s]."\",\"".$facilities[$s]."\",\"".$reviews[$s]."\")");
 }
+
  mysqli_close($dbconnect);
+
 ?>
+
